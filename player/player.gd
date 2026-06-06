6f7717c3984256beef6b3373aebf23e111a8ba1b
Rukoilla <siinai.rukoilla@gmail.com>
2026-06-06 20:54:16 +0700

    ye :(

--- player/player.gd
+++ player/player.gd
@@ -2,20 +2,22 @@ extends CharacterBody3D
 
 @export var speed = 4
 @export var gravity = 15
+@onready var camera_controller: Node3D = $"../CameraController"
 
 var target_velocity = Vector3.ZERO
+var default_controller_pos = Vector3(0.0, 5.0, 5.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	camera_controller.position = default_controller_pos
 	pass
-	
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
 	
+	# Get Input
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -27,7 +29,7 @@ func _physics_process(delta: float) -> void:
 	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-
+	
 	
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
@@ -37,13 +39,22 @@ func _physics_process(delta: float) -> void:
 		target_velocity.z = direction.z * speed * 1.4
 	
 	# Vertical Velocity
-	if Input.is_action_just_pressed("jump"):
-		direction.y += 1
+	if Input.is_action_just_pressed("jump") and is_on_floor_only():
+		direction.y = 1
 		target_velocity.y = direction.y * 5
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (gravity * delta)
 		
-
+	
 	# Moving the Character
-	velocity = target_velocity
+	velocity = lerp(velocity, target_velocity, 0.15)
 	move_and_slide()
+	
+	# Handling Camera juice
+	var cam_pos = camera_controller.get_position()
+	var cursor_pos = get_viewport().get_mouse_position()
+	
+	cam_pos.x = lerp(position.x + default_controller_pos.x, cursor_pos.x, 0.0007)
+	cam_pos.z = lerp(position.z + default_controller_pos.z, cursor_pos.y, 0.0007)
+	cam_pos.y = 5.0
+	camera_controller.position = cam_pos
