e26837ba7d58277c78c01aa987ae09bbad3d5ace
Chayut <chun.chayut@gmail.com>
2026-06-08 22:20:27 +0700

    Multi-species quests data + player state functions

--- data/quest_data.gd
+++ data/quest_data.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var id: String
 @export var quest_name: String
-@export var requested_animal_id: String
+@export var requested_animals: Dictionary[String, int] # animal id -> amount
 @export var reward: int
 @export var description: String
 @export var posted_by: String
--- globals/player_state.gd
+++ globals/player_state.gd
@@ -1,19 +1,35 @@
 extends Node
 
-var logbook: Dictionary[String, bool] = {}        # { animal_id, caught? }
+# Quest statues:
+# - AVAILABLE: not yet started (on quest board)
+# - ACTIVE: started, not caught all requied animals yet
+# - READY: completed request, ready to claim reward
+# - DONE: completed + reward claimed
+enum QuestStatus { AVAILABLE, ACTIVE, READY, DONE }
+
+var logbook: Dictionary[String, bool] = {}       # { animal_id: ever_caught? }
+var inventory: Dictionary[String, int] = {}      # { animal_id: count_held }
+var quests: Dictionary[String, QuestStatus] = {} # { quest_id: status }
+var money: int = 0
 
 
 func _ready() -> void:
-	for animal_id in AnimalRegistry.get_all_ids():
+	for animal_id in AnimalRegistry.get_all_animal_ids():
 		logbook[animal_id] = false
+		inventory[animal_id] = 0
+	for quest_id in QuestRegistry.get_all_quest_ids():
+		quests[quest_id] = QuestStatus.AVAILABLE
 	Events.animal_caught.connect(_on_animal_caught)
 
+
+# --- Animals ---
 
 func catch_animal(animal_id: String) -> void:
 	if animal_id not in logbook:
 		push_error("PlayerState: caught unknown animal id '%s'" % animal_id)
 		return
 	logbook[animal_id] = true
+	inventory[animal_id] += 1
 	Events.animal_caught.emit(animal_id)
 
 
@@ -21,6 +37,57 @@ func is_caught(animal_id: String) -> bool:
 	return logbook.get(animal_id, false)
 
 
-func _on_animal_caught(animal_id: String) -> void:
-	# flip active quest to ready
-	pass
+func get_count(animal_id: String) -> int:
+	return inventory.get(animal_id, 0)
+
+
+# --- Quests ---
+
+func start_quest(quest_id: String) -> void:
+	if not QuestRegistry.get_quest(quest_id):
+		push_error("PlayerState: trying to started a quest that doesn't exist in the registry: %s" % quest_id)
+		return
+	if quests.get(quest_id) != QuestStatus.AVAILABLE:
+		push_error("PlayerState: quest '%s' not available to start" % quest_id)
+		return
+	quests[quest_id] = QuestStatus.ACTIVE
+	_reevaluate_quest(quest_id)  # inventory may already satisfy it
+	Events.quest_started.emit(quest_id)
+
+
+func can_fulfill(quest_id: String) -> bool:
+	var quest: QuestData = QuestRegistry.get_quest(quest_id)
+	if quest == null:
+		push_error("PlayerState.can_fulfill: The quest id %s doesn't exist", quest_id)
+		return false
+	for animal_id in quest.requested_animals:
+		if get_count(animal_id) < quest.requested_animals[animal_id]:
+			return false
+	return true
+
+
+func claim_quest(quest_id: String) -> bool:
+	if quests.get(quest_id) != QuestStatus.READY:
+		push_error("PlayerState.claim_Quest: quest %s is not ready to claim", quest_id)
+		return false
+	var quest: QuestData = QuestRegistry.get_quest(quest_id)
+	for animal_id in quest.requested_animals:
+		inventory[animal_id] -= quest.requested_animals[animal_id]
+	quests[quest_id] = QuestStatus.DONE
+	money += quest.reward
+	Events.money_update.emit(money)
+	Events.quest_claimed.emit(quest_id)
+	return true
+
+
+func _reevaluate_quest(quest_id: String) -> void:
+	if quests.get(quest_id) != QuestStatus.ACTIVE:
+		return
+	if can_fulfill(quest_id):
+		quests[quest_id] = QuestStatus.READY
+		Events.quest_ready.emit(quest_id)
+
+
+func _on_animal_caught(_animal_id: String) -> void:
+	for quest_id in quests:
+		_reevaluate_quest(quest_id)
