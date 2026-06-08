ab6e09cbbbdb968a1d419675cf8ea346ef925d4e
Rukoilla <siinai.rukoilla@gmail.com>
2026-06-08 13:39:01 +0700

    Terra has been formed

--- player/player.gd
+++ player/player.gd
@@ -6,7 +6,8 @@ extends CharacterBody3D
 @onready var _animation_player = $AnimationPlayer
 
 var target_velocity = Vector3.ZERO
-var default_controller_pos = Vector3(0.0, 4.2, 4.2)
+var default_controller_pos = Vector3(0.0, 5, 5)
+var controller_pos_limit = Vector2(1.0, 5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -63,8 +64,12 @@ func _physics_process(delta: float) -> void:
 	
 	var viewport_size = get_viewport().get_visible_rect().size
 	
+	if Input.is_action_just_pressed("scroll_up") and default_controller_pos.y > controller_pos_limit.x:
+		default_controller_pos = lerp(default_controller_pos, Vector3(0.0, controller_pos_limit.x,controller_pos_limit.x), 0.3)
+	if Input.is_action_just_pressed("scroll_down") and default_controller_pos.y < controller_pos_limit.y :
+		default_controller_pos = lerp(default_controller_pos, Vector3(0.0, controller_pos_limit.y,controller_pos_limit.y), 0.3)
 
 	cam_pos.x = lerp(0.01 * velocity.x + position.x + default_controller_pos.x, cursor_pos.x - viewport_size.x / 2, 0.0002)
 	cam_pos.z = lerp(0.01 * velocity.z + position.z + default_controller_pos.z, cursor_pos.y - viewport_size.y / 2, 0.0002)
-	cam_pos.y = lerp(0.01 * velocity.y + default_controller_pos.y, position.y + default_controller_pos.y, 0.7)
+	cam_pos.y = lerp(position.y + default_controller_pos.y, position.y + default_controller_pos.y, 0.7)
 	camera_controller.position = cam_pos
